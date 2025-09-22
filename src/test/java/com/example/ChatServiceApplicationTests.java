d3a8bf8f0915bbd65259365c638d746ef23e2eed
taoxiao <taoxiao@zhilehuo.onaliyun.com>
2025-09-22 13:29:25 +0800

    优化

--- src/test/java/com/example/ChatServiceApplicationTests.java
+++ src/test/java/com/example/ChatServiceApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example;
-
 import com.example.entity.constants.Constants;
 import com.example.utils.RedisUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,5 +24,25 @@ class ChatServiceApplicationTests {
         List<String> userIds = Arrays.asList("U51006520250", "U73005020250","U73005020250","U73005020250","U73005020250","U73005020250","U73005020250","U73005020250");
         redisUtils.batchAppendOrRemoverContacts(keyPrefix, userIds, "G99626920250", 0);
     }
+
+    @Value("${project.folder}")
+    private String path;
+
+
+    @Test
+    void fun() throws IOException {
+        System.out.println(path);
+
+        // 获取目标文件路径对象
+        Path targetPath = Paths.get(path+"234234\\U73005020250");
+        // 确保目标文件的父目录存在
+        Path parentDir = targetPath.getParent();
+        boolean exists = Files.exists(parentDir);
+
+        if (parentDir != null && !exists) {
+//            Files.createDirectories(parentDir);
+        }
+    }
+
 
 }
